a5921c4a8e1a8656a994ce1c27042285dbfa054f
Andrew Sullivan <andrewcsullivan@yahoo.com>
2017-09-07 11:07:31 -0700

    Add python upload script 2

--- upload-portfolio-lamba.py
+++ upload-portfolio-lamba.py
@@ -9,7 +9,7 @@ s3 = boto3.resource("s3",config=Config(signature_version='s3v4'))
 
 portfolio_bucket = s3.Bucket('portfolio-acs')
 build_bucket = s3.Bucket('portfoliobuild-acs')
-build_bucket.download_file('portfoliobuild.zip','portfoliobuild3.zip')
+
 
 portfolio_zip = StringIO.StringIO()
 build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
